6a5f92b5cab9a40ccc3c21b06dda773b4243ae47
RachelMcNeely <mcneelyra@g.cofc.edu>
2018-10-07 20:41:39 -0400

    Delete HW1 Problem 2 Code.R

--- HW1 Problem 2 Code.R
+++
@@ -1,8 +0,0 @@
-
-library(readr)
-mydata <- read_csv("~/Math 440/mydata.csv")
-#View(mydata)
-plot(y~x,pch=19, col="red", data=mydata)
-my_lm <- lm(y~x, data = mydata)
-summary(my_lm)
-abline(my_lm)
